af3bd64433f308c5d5e3ec16a743aaebf42ef0b6
Viktoria <vikamosk@gmail.com>
2022-09-05 22:17:53 +0300

    Radio added

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,81 @@
+package ru.netology.domain;
+
+public class Radio {
+
+    private int currentRadioStation;
+    private int currentVolume;
+
+    public int getCurrentRadioStation() {
+        return currentRadioStation;
+    }
+
+    public void setCurrentRadioStation(int newCurrentRadioStation) {
+        if (0 <= newCurrentRadioStation & newCurrentRadioStation <= 9) {
+            this.currentRadioStation = newCurrentRadioStation;
+        } else {
+            return;
+        }
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume >= 0 & newCurrentVolume <= 10) {
+            this.currentVolume = newCurrentVolume;
+        } else {
+            if (newCurrentVolume < 0) {
+                this.currentVolume = 0;
+            } else {
+                this.currentVolume = 10;
+            }
+        }
+    }
+
+    public void next(int currentRadioStation) {
+        if (currentRadioStation >= 0 & currentRadioStation <= 9) {
+            if (currentRadioStation == 9) {
+                setCurrentRadioStation(0);
+            } else {
+                setCurrentRadioStation(currentRadioStation + 1);
+            }
+        } else {
+            return;
+        }
+    }
+
+    public void prev(int currentRadioStation) {
+        if (currentRadioStation >= 0 & currentRadioStation <= 9) {
+            if (currentRadioStation == 0) {
+                setCurrentRadioStation(9);
+            } else {
+                setCurrentRadioStation(currentRadioStation - 1);
+            }
+        } else {
+            return;
+        }
+    }
+
+    public void increaseVolume(int currentVolume) {
+        if (currentVolume >= 0 & currentVolume <= 10) {
+            if (currentVolume == 10) {
+                setCurrentVolume(currentVolume);
+            } else {
+                setCurrentVolume(currentVolume + 1);
+            }
+        } else {
+            return;
+        }
+    }
+
+    public void decreaseVolume(int currentVolume) {
+        if (currentVolume >= 0 & currentVolume <= 10) {
+            if (currentVolume == 0) {
+                return;
+            } else {
+                setCurrentVolume(currentVolume - 1);
+            }
+        }
+    }
+}
---
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -0,0 +1,50 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.Assert.assertEquals;
+
+public class RadioTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dataNextRadioStation.csv")
+    void nextRadioStation (int currentRadioStation, int expected) {
+        Radio radio = new Radio();
+        radio.next(currentRadioStation);
+        int actual = radio.getCurrentRadioStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dataPrevRadioStation.csv")
+    void prevRadioStation (int currentRadioStation, int expected) {
+        Radio radio = new Radio();
+        radio.prev(currentRadioStation);
+        int actual = radio.getCurrentRadioStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dataIncreaseVolume.csv")
+    void increaseVolumeTest (int currentVolume, int expected) {
+        Radio radio = new Radio();
+        radio.increaseVolume(currentVolume);
+        int actual = radio.getCurrentVolume();
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dataDecreaseVolume.csv")
+    void decreaseVolumeTest (int currentVolume, int expected) {
+        Radio radio = new Radio();
+        radio.decreaseVolume(currentVolume);
+        int actual = radio.getCurrentVolume();
+
+        assertEquals(expected, actual);
+    }
+}
